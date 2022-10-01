ddf0cf321d9581ccc084143560d6318817405025
Артур <arthur@blackbricks.io>
2022-10-01 22:28:14 +0600

    Initial Commit

---
+++ DesignCodeWeatherApp/DesignCodeWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeWeatherAppApp.swift
+//  DesignCodeWeatherApp
+//
+//  Created by Arthur on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
